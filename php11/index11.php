8fc4c50eb02e0b56a04a03a80103ae0adf2fd2d5
ReKaSan <redaskardauskas@gmail.com>
2019-08-17 23:38:48 +0300

    php11 pataisytas

--- php11/index11.php
+++ php11/index11.php
@@ -10,21 +10,25 @@
 </head>
 
 <body>
+
+    <?php
+    if(isset($_POST['submit'])){
+    if($_REQUEST['aukstis'] && $_REQUEST['plotis']) {
+         $plotas = $_POST['aukstis'] * $_POST['plotis'];
+        echo 'Stačiakampio plotas: ' . $plotas. 'kv.m';
+        exit();
+    }
+    else {
+        die ("Užpildykite visas eilutes");
+    }
+    }
+    ?>
+
     <form action="<?php $_PHP_SELF; ?>" method="POST">
         Aukštis: <input type="text" name="aukstis" value=""/>
         Plotis: <input type="text" name="plotis" value=""/>
         <input type="submit" name="submit" value="Skaiciuoti">
     </form>
-    <?php
-
-    if($_REQUEST['aukstis'] && $_REQUEST['plotis']) {
-         $plotas = $_POST['aukstis'] * $_POST['plotis'];
-        echo 'Stačiakampio plotas: ' . $plotas. 'kv.m';
-    }
-    else {
-        echo "Užpildykite visas eilutes";
-    }
-    ?>
-    </body>
+</body>
 
 </html>
