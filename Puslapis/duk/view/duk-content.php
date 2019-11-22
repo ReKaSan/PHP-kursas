99a0c3a1fcb8727ffcb74c16fb7079e0c304878e
ReKaSan <redaskardauskas@gmail.com>
2019-11-22 23:41:09 +0200

    PHP puslapis

---
+++ Puslapis/duk/view/duk-content.php
@@ -0,0 +1,36 @@
+<main>
+    <div class="row">
+        <div class="col l7 s12" style="height: 615px;">
+            <img class="img" src="/../php/PHP-kursas/Puslapis/forma/app/img/breastfeeding4e.jpg">
+        </div>
+        <div class="col l5 s12">
+            <article>
+                <h2>D. U. K.</h2>
+            </article>
+            <ul class="collapsible">
+                <li>
+                    <div class="collapsible-header"><i class="material-icons">check</i>Prietaisas neveikia</div>
+                    <div class="collapsible-body"><span>Parikrinkite baterijas (jei jas naudojate, kaip paildomą maitinimo šaltinį).</span></div>
+                    <div class="collapsible-body"><span>Parikrinkite maitinimo blokelį.</span></div>
+                    <div class="collapsible-body"><span>Prijunkite maitinimo blokelį prie kito kištukinio lizdo (rozetės).</span></div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><i class="material-icons">check</i>Suprastėjo traukimas arba pradingo visiškai</div>
+                    <div class="collapsible-body"><span>Patikrinkite baltą membraną, kuri įstatoma į geltoną kaušelį. Esant membranos įtrūkimui pakeiskite ją.</span></div>
+                    <div class="collapsible-body"><span>Patikrinkite geltoną kaušelį, į kurį įsistato membrana. Esant pažeidimams pakeiskite jį.</span></div>
+                    <div class="collapsible-body"><span>Patikrinkite geltono kaušelio pavišių, ar nėra susikaupusių riebalinių apnašų. Nuimkite membraną, nuplaukite nešvarumus ir nusausinkite.</span></div>
+                    <div class="collapsible-body"><span>Atkreipkite dėmesį į prietaiso variklio darbo intensyvumą bei tolygumą.</span></div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><i class="material-icons">check</i>Į prietaisą pateko skyščių</div>
+                    <div class="collapsible-body"><span>Nedelsiant išjunkite pientraukį.</span></div>
+                    <div class="collapsible-body"><span>Jei į pientraukį pateko daug skyščių atlikite plovimo procedūrą vadovaudamiesi nuorodomis pateiktomis vartotojo vadove.</span></div>
+                </li>
+                <li>
+                    <div class="collapsible-header"><i class="material-icons">check</i>Negalite nustatyti gedimo priežasties</div>
+                    <div class="collapsible-body"><span>Susisiekite su įgliotu gamintojo atstovu nurodytais kontaktais (žr. Kontaktai).</span></div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</main>
---
+++ Puslapis/forma/app/views/content.php
@@ -0,0 +1,23 @@
+<main>
+    <div class="row">
+      <div class="col l7 s12" style="height: 615px" ;>
+        <img class="img" src="/../php/PHP-kursas/Puslapis/forma/app/img/breastfeeding4e.jpg">
+      </div>
+      <div class="col l5 s12" style="padding: 30px;">
+        <form method="post">
+          <div class="label">Vardas</div>
+          <input type="text" name="vardas" placeholder="Jūsų vardas.." required autofocus>
+
+          <div class="label">Pavardė</div>
+          <input type="text" name="pavarde" placeholder="Jūsų pavardė.." required>
+
+          <div class="label">El. paštas</div>
+          <input type="text" name="email" placeholder="Jūsų el. paštas.." required>
+
+          <div class="label">Komentaras</div>
+          <textarea name="message" placeholder="Jūsų komentaras.." style="height:200px" required></textarea>
+          <input name="submit" type="submit" value="Siųsti">
+        </form>
+      </div>
+    </div>
+  </main>
